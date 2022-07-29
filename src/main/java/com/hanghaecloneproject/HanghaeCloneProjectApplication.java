d0f9f7a6cc6017c3844f7ec1b8be3f9798a93cce
9JaHyun <jahyunnine@gmail.com>
2022-07-29 19:04:41 +0900

    Init: 스프링 프로젝트 생성 - .gitignore 설정 완료 - DEV 전용 application.yaml 분리

---
+++ src/main/java/com/hanghaecloneproject/HanghaeCloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hanghaecloneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeCloneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeCloneProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghaecloneproject/HanghaeCloneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghaecloneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeCloneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
